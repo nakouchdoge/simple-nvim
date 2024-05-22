102b96f0172fd4cac0f761879b920e4fd97d7ab8
nakouchdoge <118134381+nakouchdoge@users.noreply.github.com>
2024-05-22 00:37:01 -0500

    Add files via upload

---
+++ lua/tn/themes.lua
@@ -0,0 +1,34 @@
+return {
+	{ "tiagovla/tokyodark.nvim" },
+	{ "navarasu/onedark.nvim", style = "deep", name = "onedark", },
+	{ "neanias/everforest-nvim", style = "hard"},
+	{ "rebelot/kanagawa.nvim" },
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "Mofiqul/dracula.nvim" },
+	{ "EdenEast/nightfox.nvim" },
+	{
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            require("tokyonight").setup({
+				lazy = false,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = true, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "dark", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+            })
+		vim.cmd("colorscheme tokyonight")
+
+        end
+    },
+}}
---
+++ lua/vimscript.lua
@@ -0,0 +1,6 @@
+vim.cmd("set number relativenumber")
+vim.cmd("set cursorline")
+vim.cmd("set guicursor=a:blinkon100")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set scrolloff=10")
